7f3b6c43b467113d0b61dba580d780bc9f65ca4d
Behrouz87RN <112555779+Behrouz87RN@users.noreply.github.com>
2023-07-18 21:29:19 +0200

    add comment  foe cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
+# Check if a Tag is provided as an argument
 if [ -z "$1" ]; then
   echo "Usage: $0 <Tag>"
   exit 1
 fi
 
-Tag="$2"
-RouterName="${Tag}_router"
+# Read the provided Tag from the command-line argument
+Tag="$1"
+router_name="${Tag}_router"
 
-
-KeypairName=$(openstack keypair list  -f value -c Name | grep $Tag)
-if [ -n "$KeypairName" ]; then
-  
-  openstack keypair delete "$KeypairName"
+# Check if a keypair with the specified Tag exists and delete it
+keypair_name=$(openstack keypair list -f value -c Name | grep $Tag)
+if [ -n "$keypair_name" ]; then
+  openstack keypair delete "$keypair_name"
   if [ $? -eq 0 ]; then
-    echo "Tag '$Tag' removed from keypair '$KeypairName'."
+    echo "Tag '$Tag' removed from keypair '$keypair_name'."
   else
-    echo "Failed to remove Tag '$Tag' from keypair '$KeypairName'."
+    echo "Failed to remove Tag '$Tag' from keypair '$keypair_name'."
   fi
 else
   echo "No keypair found with Tag '$Tag'."
 fi
 
-
-FloatingIps=$(openstack floating ip list -f value -c ID)
-if [ -n "$FloatingIps" ]; then
+# Check and remove floating IP addresses
+floating_ips=$(openstack floating ip list -f value -c ID)
+if [ -n "$floating_ips" ]; then
   while read -r floating_ip; do
     openstack floating ip delete "$floating_ip"
     if [ $? -eq 0 ]; then
@@ -32,14 +33,14 @@ if [ -n "$FloatingIps" ]; then
     else
       echo "Failed to delete floating IP '$floating_ip'."
     fi
-  done <<< "$FloatingIps"
+  done <<< "$floating_ips"
 else
   echo "No floating IP addresses found."
 fi
 
-
-ServerNames=$(openstack server list --name "^${Tag}*" -f value -c Name)
-if [ -n "$ServerNames" ]; then
+# Delete servers with names starting with the Tag
+server_names=$(openstack server list --name "^${Tag}*" -f value -c Name)
+if [ -n "$server_names" ]; then
   while read -r server_name; do
     openstack server delete "$server_name"
     if [ $? -eq 0 ]; then
@@ -47,26 +48,26 @@ if [ -n "$ServerNames" ]; then
     else
       echo "Failed to delete server '$server_name'."
     fi
-  done <<< "$ServerNames"
+  done <<< "$server_names"
 else
   echo "No servers found with names starting with '$Tag'."
 fi
 
-openstack router show "$RouterName" > /dev/null 2>&1
+# Check if the router exists
+openstack router show "$router_name" > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-  
   subnet_name="${Tag}_subnet"
 
-
-  openstack router remove subnet "$RouterName" "$subnet_name"
+  # Disconnect subnet from router
+  openstack router remove subnet "$router_name" "$subnet_name"
   if [ $? -eq 0 ]; then
-    echo "Subnet '$subnet_name' disconnected from router '$RouterName'."
+    echo "Subnet '$subnet_name' disconnected from router '$router_name'."
   else
-    echo "Failed to disconnect subnet '$subnet_name' from router '$RouterName'."
+    echo "Failed to disconnect subnet '$subnet_name' from router '$router_name'."
     exit 1
   fi
 
-
+  # Delete the subnet
   openstack subnet delete "$subnet_name"
   if [ $? -eq 0 ]; then
     echo "Subnet '$subnet_name' deleted."
@@ -74,29 +75,28 @@ if [ $? -eq 0 ]; then
     echo "Failed to delete subnet '$subnet_name'."
   fi
 
-
-  openstack router delete "$RouterName"
+  # Delete the router
+  openstack router delete "$router_name"
   if [ $? -eq 0 ]; then
-    echo "Router '$RouterName' deleted."
+    echo "Router '$router_name' deleted."
   else
-    echo "Failed to delete router '$RouterName'."
+    echo "Failed to delete router '$router_name'."
   fi
 else
-  echo "Router '$RouterName' not found."
+  echo "Router '$router_name' not found."
 fi
 
-
-NetworkName="${Tag}_network"
-openstack network delete "$NetworkName" > /dev/null 2>&1
+# Delete the network
+network_name="${Tag}_network"
+openstack network delete "$network_name" > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-  echo "Network '$NetworkName' deleted."
+  echo "Network '$network_name' deleted."
 else
-  echo "Failed to delete network '$NetworkName' or network not found."
+  echo "Failed to delete network '$network_name' or network not found."
 fi
 
-
-RemainingNetworks=$(openstack network list --Tags "$Tag" -f value -c ID)
-if [ -z "$RemainingNetworks" ]; then
-  echo "No networks found with this Tag '$Tag'."
+# Check if there are any networks remaining
+remaining_networks=$(openstack network list --Tags "$Tag" -f value -c ID)
+if [ -z "$remaining_networks" ]; then
+  echo "No networks found with Tag '$Tag'."
 fi
-
